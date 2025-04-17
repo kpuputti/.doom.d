678cde2e93d6da969fe07c3210063c17dbe86602
Kimmo Puputti <git@kimmo.kpuputti.fi>
2025-04-17 20:06:43 +0300

    clean up config, add gql fragment polymode

--- config.el
+++ config.el
@@ -1,57 +1,10 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Kimmo Puputti"
       user-mail-address "emacs@kimmo.kpuputti.fi")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-vibrant)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 
 (defconst is-mac (eq system-type 'darwin))
 
@@ -75,14 +28,10 @@
                                       (abbreviate-file-name (buffer-file-name))
                                     "%b")))
 
-      ; https://github.com/hlissner/doom-emacs/issues/4158
+                                        ; https://github.com/hlissner/doom-emacs/issues/4158
       +format-with-lsp nil)
 
-;; Start initial Emacs frame in fullscreen
-;;(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-(after! undo-tree
-  (setq undo-tree-enable-undo-in-region t))
+(after! undo-tree (setq undo-tree-enable-undo-in-region t))
 
 (after! whitespace
   (setq whitespace-style '(face tabs tab-mark trailing empty))
@@ -92,10 +41,43 @@
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
         magit-bury-buffer-function  #'magit-restore-window-configuration))
 
-(after! anzu
-  (global-anzu-mode +1))
+(after! anzu (global-anzu-mode +1))
 
 (map! "C-a" #'beginning-of-line
       "M-o" #'ace-window
       "M-/" #'hippie-expand
       "C-c b" #'+default/new-buffer)
+
+(add-to-list 'auto-mode-alist '("\\.sqlx\\'" . sql-mode))
+
+;; accept completion from copilot and fallback to company
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+
+(use-package! graphql-mode
+  :defer t)
+
+(after! polymode
+  (require 'graphql-mode)
+
+  (define-hostmode typescript-hostmode
+    :mode 'typescript-mode)
+
+  (define-innermode graphql-innermode
+    :mode 'graphql-mode
+    :head-matcher "gql`"
+    :tail-matcher "`"
+    :head-mode 'host
+    :tail-mode 'host)
+
+  (define-polymode poly-typescript-mode
+    :hostmode 'typescript-hostmode
+    :innermodes '(graphql-innermode))
+
+  (add-to-list 'auto-mode-alist
+               '("\\.ts\\'" . poly-typescript-mode)))
